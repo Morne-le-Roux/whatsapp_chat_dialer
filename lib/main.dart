5b48940914f0ef73ed6f6222d052d4dffd45c5da
Morné le Roux <mornelerx@gmail.com>
2024-03-10 11:32:46 +0200

    reskin and prep

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows) {
+    WindowManager.instance.setMinimumSize(const Size(400, 600));
+    WindowManager.instance.setMaximumSize(const Size(400, 600));
+  }
   runApp(const WhatsappChatDialer());
 }
 
@@ -14,16 +22,18 @@ class WhatsappChatDialer extends StatefulWidget {
 
 class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
   //VARIABLES
-  String countryCode = "";
-  String phoneNumber = "";
-  String api = "https://wa.me/";
-  String completedLink = "";
+
+  final TextStyle _textStyle =
+      const TextStyle(color: Colors.amber, fontWeight: FontWeight.bold);
+
+  String _phoneNumber = "";
+  String _completedLink = "";
+  final TextEditingController _countryCode = TextEditingController();
 
 //FUNCTION TO LAUNCH WHATSAPP URL
   Future<void> launchWhatsapp({required url}) async {
     if (await canLaunchUrl(url)) {
       await launchUrl(url);
-      print("launched");
     } else {
       throw "Could not launch $url";
     }
@@ -31,25 +41,33 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
 
   @override
   Widget build(BuildContext context) {
+    _countryCode.text = "27";
     return MaterialApp(
       home: Builder(builder: (context) {
         return Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.greenAccent.shade700,
-            title: const Center(child: Text("Whatsapp Chat Dialer")),
+            backgroundColor: Colors.amber,
+            title: const Center(
+                child: Text(
+              "PIC Whatsapp Dialer",
+              style: TextStyle(fontWeight: FontWeight.w900),
+            )),
           ),
 
 //BODY
 
           body: Container(
 //BACKGROUND COLOR
-            color: Colors.white,
+            color: const Color.fromARGB(255, 33, 0, 37),
             child: Column(
               children: [
                 const SizedBox(
                   height: 20,
                 ),
-                const Text("Country Code"),
+                Text(
+                  "Country Code",
+                  style: _textStyle,
+                ),
 
 //COUNTRY CODE TEXTFIELD
 
@@ -61,22 +79,29 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
                     decoration: BoxDecoration(
                         border: Border.all(
 //BORDER COLOR
-                            color: Colors.lightGreenAccent),
+                            color: Colors.amber),
                         borderRadius:
                             const BorderRadius.all(Radius.circular(10))),
                     child: TextField(
+                      style: _textStyle,
+                      controller: _countryCode,
                       decoration: const InputDecoration(
-                          icon: Icon(Icons.map_outlined),
+                          icon: Icon(
+                            Icons.map_outlined,
+                            color: Colors.amber,
+                          ),
                           prefix: Text("+"),
                           border: InputBorder.none),
-                      onChanged: (value) => countryCode = value,
                     ),
                   ),
                 ),
                 const SizedBox(
                   height: 20,
                 ),
-                const Text("Phone Number"),
+                Text(
+                  "Phone Number",
+                  style: _textStyle,
+                ),
 
 //PHONE NUMBER TEXTFIELD
 
@@ -88,34 +113,40 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
                     decoration: BoxDecoration(
                         border: Border.all(
                             //BORDER COLOR
-                            color: Colors.lightGreenAccent),
+                            color: Colors.amber),
                         borderRadius:
                             const BorderRadius.all(Radius.circular(10))),
                     child: TextField(
+                      style: _textStyle,
                       decoration: const InputDecoration(
-                          icon: Icon(Icons.phone_outlined),
+                          icon: Icon(
+                            Icons.phone_outlined,
+                            color: Colors.amber,
+                          ),
                           border: InputBorder.none),
-                      onChanged: (value) => phoneNumber = value,
+                      onChanged: (value) => _phoneNumber = value,
                     ),
                   ),
                 ),
 
 //BUTTON TO LAUNCH URL
 
+                const SizedBox(height: 20),
+
                 GestureDetector(
                   child: Container(
                     height: 50,
                     width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.lightGreenAccent.shade700,
-                      borderRadius: const BorderRadius.all(
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(
                         Radius.circular(10),
                       ),
                     ),
-                    child: const Center(
+                    child: Center(
                       child: Text(
                         "Open Chat",
-                        style: TextStyle(color: Colors.white),
+                        style: _textStyle.copyWith(color: Colors.black),
                       ),
                     ),
                   ),
@@ -123,33 +154,34 @@ class _WhatsappChatDialerState extends State<WhatsappChatDialer> {
 //ON TAP
 
                   onTap: () {
-                    completedLink = "$api$countryCode$phoneNumber";
+                    _completedLink =
+                        "whatsapp://send/?phone=${_countryCode.text}$_phoneNumber&text&type=phone_number";
 
 //CONDITIONALS TO CHECK
 
 //IF NO PHONE NUMBER IS INSERTED
-                    if (phoneNumber == "") {
+                    if (_phoneNumber == "") {
                       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                           content: Text(
                               "You cannot leave the phone number empty.")));
 
 //IF NO COUNTRY CODE IS INSERTED
                     } else {
-                      if (countryCode == "") {
+                      if (_countryCode.text == "") {
                         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                             content: Text(
                                 "You cannot leave the country code empty.")));
 
 //IF THERE IS A ZERO IN FRONT OF THE NUMBER
                       } else {
-                        if (phoneNumber.substring(0, 1) == "0") {
+                        if (_phoneNumber.substring(0, 1) == "0") {
                           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                               content: Text(
                                   "Please remove the '0' in front of your phone number!")));
 
 //IF EVERYTHING ELSE RUNS FINE
                         } else {
-                          var uri = Uri.parse(completedLink);
+                          var uri = Uri.parse(_completedLink);
                           launchWhatsapp(url: uri);
                         }
                       }
